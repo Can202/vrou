5237b3dfff44f94ee6bceec463eee9a892e33549
Can202 <mgoopazo@gmail.com>
2021-09-13 09:05:23 -0300

    test

--- src/vrou.py
+++ src/vrou.py
@@ -2,8 +2,19 @@ import sys
 import os
 from sys import exit
 
+INSTALL="install"
+INSTALL2="in"
+INSTALL3="-S"
+PROGRAM=""
 def main():
-	print("h")
+	print("Welcome to vrou")
+	for i in range(len(sys.argv)):
+		if sys.argv[i] == INSTALL or sys.argv[i] == INSTALL2 or sys.argv[i] == INSTALL3:
+			print("install")
+			if len(sys.argv) <= i+1:
+				print("program")
+			else:
+				PROGRAM = str(sys.argv[i+1])
 
 if __name__ == "__main__":
 	main()
