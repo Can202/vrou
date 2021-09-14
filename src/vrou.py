0acc37201b9f4f5b89df4b2210d20f0603395de5
Can202 <mgoopazo@gmail.com>
2021-09-13 23:31:40 -0300

    test

--- src/vrou.py
+++ src/vrou.py
@@ -29,17 +29,52 @@ def main():
 					installpak +=1
 
 def install(program = "none"):
+
+	extra=""
+	# Detect snap
+	if os.path.exists("/usr/bin/snap"):
+		extra += " snap "
+	#Detect flatpak
+	if os.path.exists("/usr/bin/flatpak"):
+		extra += " flatpak "
+
+
+	#Detect Choco
+	if os.path.exists("C:\\ProgramData\\chocolatey\\bin\\choco.exe"):
+		print("choco detect")
+		cpm = "choco"
+		os.system("powershell -Command choco install " + program)
+
+	#Detect zypper
 	if os.path.exists("/usr/bin/zypper"):
-		os.system("zypper in " + program)
+		os.system("zypper in -n " + program)
+		cpm = "zypper"
+
+	#Detect apt
 	if os.path.exists("/usr/bin/apt"):
 		os.system("apt install -y " + program)
+		cpm = "apt"
+
+	# Detect dnf
 	if os.path.exists("/usr/bin/dnf"):
 		os.system("dnf -y install " + program)
+		cpm = "dnf"
+
+	#Detect pacman
 	if os.path.exists("/usr/bin/pacman"):
 		os.system("pacman -S " + program)
-	if os.path.exists("/usr/bin/snap"):
-		os.system("snap install " + program)
-	if os.path.exists("/usr/bin/flatpak"):
-		os.system("flatpak install flathub " + program)
+		cpm = "pacman"
+
+	if extra != "":
+		if "snap" in extra:
+			print("you can use snap, or your current package manager (" + cpm + ")")
+			anr = input("Use snap? (Y/n)")
+			if "Y" in anr or "y" in anr:
+				os.system("snap install " + program)
+		if "flatpak" in extra:
+			print("you can use flatpak, or your current package manager (" + cpm + ")")
+			anr = input("Use snap? (Y/n)")
+			if "Y" in anr or "y" in anr:
+				os.system("flatpak install flathub " + program)
 if __name__ == "__main__":
 	main()
